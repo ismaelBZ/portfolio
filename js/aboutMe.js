36b5fe73ac2d7869a2153f83605c08807a99fece
Ismael <ismaelbetzoletti@gmail.com>
2023-11-01 17:34:40 -0300

    fixin abou-me auto adjust hight

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -3,9 +3,29 @@ const display = window.getComputedStyle(aboutMe).display;
 const mobileBtn = window.document.querySelector("#aboutMobile");
 const desktopBtn = window.document.querySelector("#aboutDesktop");
 const aboutInfo = window.document.querySelectorAll(".about-info");
+const devNameAndOccupation = window.document.querySelector(".dev-name-occupation")
+const aboutMeInfoWrapper = window.document.querySelector(".about-me-info-wrapper")
 
-// ABOUT ME HEIGHT
+// ABOUT ME HEIGHT SETTINGS
+
 aboutMe.style.height = `${window.document.scrollingElement.offsetHeight + 50}px`;
+aboutMeInfoWrapper.style.minHeight = `${parseInt(window.getComputedStyle(devNameAndOccupation).height) + 50}px`
+
+function adjustDivWrapperHeigth() {
+  aboutMe.style.height = `${window.document.scrollingElement.offsetHeight + 50}px`
+}
+function adjustAboutMeInfoWrapperHeight() {
+  aboutMeInfoWrapper.style.minHeight = `${ 
+                                      parseInt(window.getComputedStyle(devNameAndOccupation).height) + 50
+                                    }px`
+  console.log(`Adjust the height`);
+}
+
+window.onresize = adjustDivWrapperHeigth;
+window.onresize = adjustAboutMeInfoWrapperHeight;
+
+
+// SHOW/OPEN ABOUT ME
 
 function showAboutMe() {
   const aboutMe = window.document.querySelector(".about-me");
@@ -19,19 +39,16 @@ function showAboutMe() {
   }
 }
 
-function adjustDivWrapperHeigth() {
-  aboutMe.style.height = `${window.document.scrollingElement.offsetHeight + 50}px`
-  console.log(`${(window.document.scrollingElement.offsetHeight)}px`);
-}
-
 mobileBtn.onclick = showAboutMe;
 desktopBtn.addEventListener("click", showAboutMe)
 
-window.onresize = adjustDivWrapperHeigth;
+// HIDE/CLOSE ABOUT ME 
 
-window.onclick = function(event) {
+window.onclick = function(event) { 
   if (event.target === aboutMe) {
     aboutMe.style.display = "none"
   }
 }
+
+
 
