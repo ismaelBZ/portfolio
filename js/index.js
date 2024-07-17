ed1b001dba43f3dbe92385beca9ed0b87f98debd
Ismael <ismaelbetzoletti@gmail.com>
2024-07-17 19:58:21 -0300

    changed font colors and add pay-api project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-window.addEventListener('click', (e) => console.log(e));
+const homeLink = document.querySelector(".home");
+const catalogLink = document.querySelector(".catalog");
+const aboutLink = document.querySelector(".about");
+const contactLink = document.querySelector(".contact");
+
+homeLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("").offsetTop;
+});
+catalogLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("").offsetTop;
+});
+aboutLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("about").offsetTop;
+});
+contactLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("contact").offsetTop;
+});
